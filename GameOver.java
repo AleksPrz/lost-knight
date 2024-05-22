ee26f54c31bcb04bf29a335cbcb0a9ed6f6a90c8
AleksPrz <144571895+AleksPrz@users.noreply.github.com>
2024-05-21 22:54:17 -0600

    Update GameOver.java

--- GameOver.java
+++ GameOver.java
@@ -6,7 +6,6 @@ public class GameOver extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(800, 600, 1);
         addObject(new Sound(), 0, 0);
-        prepare();
     }
 
     public void act()
